4e36ea396cfa7c7a955a605d8aa11203075a6e84
dogofsunrising <138022726+dogofsunrising@users.noreply.github.com>
2023-10-17 17:19:11 +0900

    森羅万象

--- app/src/main/java/com/example/travelbookmarkapp/MainActivity.kt
+++ app/src/main/java/com/example/travelbookmarkapp/MainActivity.kt
@@ -27,7 +27,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+}//うおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+//俺は人間をやめるぞおおおおおお！！！！！
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
